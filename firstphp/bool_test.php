665c436d4ee2d6590d4040b2771b73d631360154
PHP YeShao <2358365507@qq.com>
2020-11-22 21:25:16 +0800

    rename all files and learn something new

---
+++ firstphp/bool_test.php
@@ -0,0 +1,23 @@
+<pre>
+<?php
+$sex = true;
+$sex = false;
+
+$student1 = [
+    'name' => 'lsy',
+    'sex' => false,
+    'age' => 13,
+    'birthday' => '2007-09-22'
+];
+
+$student2 = [
+    'name' => 'yeshao',
+    'sex' => true,
+    'age' => 13,
+    'birthday' => '2007-03-08'
+];
+
+var_dump($student1);
+var_dump($student2);
+?>
+</pre>
---
+++ firstphp/link_symbol_and_=.php
@@ -0,0 +1,10 @@
+<?php
+$name1 = 'YeShao';
+echo $name1 . PHP_EOL;
+
+$name1 = 'lsy';
+echo $name1 . PHP_EOL;
+
+$lovesentence = 'Love';
+$yeshao = 'yeshao';
+echo $yeshao . ' ' . $lovesentence . ' ' . $name1;
