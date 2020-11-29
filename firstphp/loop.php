ad5c32bc1669226c286a77f8919b06a9c612f02b
PHP YeShao <2358365507@qq.com>
2020-11-29 21:27:51 +0800

    learn loop(1) and develop echo

---
+++ firstphp/loop.php
@@ -0,0 +1,6 @@
+<?php
+$hello = "<a href='http://129.211.88.87'>Hello</a>";
+echo $hello . PHP_EOL;
+for ($i=0; $i <20 ; $i++) {
+    echo $i . PHP_EOL;
+}
---
+++ firstphp/探究性学习.php
@@ -0,0 +1,10 @@
+<?php
+//输出 HTML <a> 标签
+$target = '<a href=\'http://129.211.88.87\'>我的服务器</a>';
+echo $target . PHP_EOL;
+
+//输出 HTML 字体
+$bold = '<b>叶少</b>';
+echo $bold . " Love" . PHP_EOL;
+$font2 = "<i>Luo&nbsp;Shengyun</i>";
+echo $font2 . PHP_EOL;
